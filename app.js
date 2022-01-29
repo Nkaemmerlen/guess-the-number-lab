7d2922b6e5284b0f36df1c526513414b5eac54eb
Nkaemmerlen <nathankaemmerlen@yahoo.com>
2022-01-29 12:43:32 -0600

    Complete with notes

--- app.js
+++ app.js
@@ -20,27 +20,57 @@ const game = {
   secretNum: null,
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.largestNum - this.smallestNum + 1)) + this.smallestNum
-      return this.secretNum;
+    (this.largestNum - this.smallestNum + 1)) + this.smallestNum
+      // return this.secretNum;
+    do {
+      this.prevGuesses.push(this.getGuess())
+      this.highLowRight()
+      //^ DO push the previous guesses into the array and run the highLowRight function to compare the current guess to the secretNum
+    } 
+      while (this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
+      //^ DO above while current guess doesnt equal the secret num
+      // return this.sercretNum
     },
     getGuess: function() {
-      let guess = null
-
-      while (isNaN (guess) || guess < this.smallestNum || guess > this.largestNum) {
-        guess = parseInt(prompt(`'Enter a guess between' ${this.smallestNum} 'and' ${this.largestNum}`))
+      let originalGuess = 0
+      //^ assigning the original guess a value of 0 to start the functions
+      while (originalGuess < this.smallestNum || originalGuess > this.largestNum || isNaN (originalGuess)) {
+        //^WHILE the guess is less than the smallest number possible, bigger than the biggest number possible, and is a string do below 
+        originalGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.largestNum}`))
+        // prompting the user to enter in an appropriate guess that abides by the set rules
       }
-      return guess
+      return originalGuess
+      // console.log(game.prevGuesses.length)
     }, 
-    
+    highLowRight: function() {
+      //^ the function to compare the guess to the random number. if it is right/low/high
+      let txt = ''
+      //^assigning text to an empty string
+      if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+        //^ IF the last inserted guess equals the secret number
+        txt = `YAY! You guessed correctly! It took you ${this.prevGuesses.length} tries!`
+        alert(txt)
+        //^ re assigning txt to the you won message and sending out a browser alert()
+      } 
+      else if (this.prevGuesses[this.prevGuesses.length -1] > this.secretNum){
+        //^ comapring the last inserted guess to the secret number and if it is higher
+        txt = `your guess was too ⇧HIGH⇧, here are you previous guesses: ${this.prevGuesses}`
+        alert(txt)
+        //^ Re assigning txt to the higher message and sending a browser alert if it is along with the prev guesses
+      } 
+      else {
+        txt = `Your guess was too ⇩LOW⇩, here are your previous guesses: ${this.prevGuesses}` 
+        alert(txt)
+        //^ if it is not equal to or higher by proccess of elimination it is lower and elert too low with the prev guesses
+      }
+    }
   }
+
+
 game.prevGuesses = []
 
 
-
-
-
-
-game.getGuess()
+game.play()
 
 
 
@@ -48,4 +78,4 @@ game.getGuess()
 
 
 
-console.log(game)
+//console.log(game)
