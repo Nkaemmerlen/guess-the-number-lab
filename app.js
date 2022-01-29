26ef3fa5c1b05758a5d607f8b7f2718c85fb7a2a
Nkaemmerlen <nathankaemmerlen@yahoo.com>
2022-01-28 21:51:29 -0600

    Add the num check

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
+// FEATURES FOR THE GAME
+// Allow the player to continually be prompted to enter their guess at the secret number until they guess correctly
+// IF there is a wrong guess tell them if it was too high or too low
+// IF guessed correctly display an alert and congratulate them, along with how many guesses it took to guess the number
+
+// TASKS
+// add a prevGuesses property to the game obj initialized to an empty array
+// add getGuess method to game that prompts the player to enter a guess "Enter a guess between [smallestNum] and [biggestNum]"
+//ensure that the getGuess method returns a value that isnt a string and is between the smallest and biggest number
+
+
+
+
+
+
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  largestNum: 100,
   smallestNum: 1,
   secretNum: null,
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.largestNum - this.smallestNum + 1)) + this.smallestNum
+      return this.secretNum;
+    },
+    getGuess: function() {
+      let guess = null
+
+      while (isNaN (guess) || guess < this.smallestNum || guess > this.largestNum) {
+        guess = parseInt(prompt(`'Enter a guess between' ${this.smallestNum} 'and' ${this.largestNum}`))
+      }
+      return guess
+    }, 
+    
   }
-}
+game.prevGuesses = []
+
+
+
+
+
+
+game.getGuess()
+
+
+
+
+
+
+
+console.log(game)
